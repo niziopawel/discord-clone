5e9680727b9a7246194ce9ea8a703880a40fe94a
niziopawel <niziopawel2@gmail.com>
2022-01-25 12:22:38 +0100

    add basic styles to devise forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users,
+             path: '',
+             path_names: {
+               sign_in: 'login',
+               sing_out: 'logout'
+             }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do
+    authenticated { root to: 'home#index', as: :authenticated_root }
+    unauthenticated { root to: 'devise/sessions#new', as: :unauthenticated_root }
+  end
 end
