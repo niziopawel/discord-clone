55f417b3ce4b3a5beb327a63c416aa7b819e92ff
niziopawel <niziopawel2@gmail.com>
2022-02-16 12:09:58 +0100

    add updating and removing channels

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -4,7 +4,8 @@ class ChannelsController < ApplicationController
   include ChannelsHelper
   before_action :set_channel, only: %i[show update edit destroy]
   before_action :set_server, only: %i[new create]
-  before_action -> { @server.server_owner?(current_user) }, only: %i[create update destroy]
+  before_action :require_permission, only: %i[new create edit update destroy]
+
 
   def show
     @server = @channel.server
@@ -34,11 +35,39 @@ class ChannelsController < ApplicationController
 
   def edit; end
 
-  def update; end
+  def update
+    if @channel.update(channel_params)
+      redirect_to channel_path(@channel), notice: 'Channel updated successfully.'
+    else
+      render(
+        turbo_stream: turbo_stream.update('channel_form', partial: 'channel/form', locals: { channel: @channel }),
+        status: :unprocessable_entity
+      )
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @server = @channel.server
+    @channel.destroy
+
+    respond_to do |format|
+      format.turbo_stream do
+        redirect_to channel_path(@server.general_channel), status: :see_other, notice: 'Channel was successfully destroyed.'
+      end
+    end
+  end
 
   private
+
+  def require_permission
+    if action_name == 'new' || action_name == 'create'
+      return if @server.owner == current_user
+    elsif @channel.server.owner == current_user
+      return
+    end
+
+    redirect_to channel_path(@channel), status: :unauthorized, notice: 'You do not have permission to do that.'
+  end
 
   def set_server
     @server = Server.find(params[:server_id])
